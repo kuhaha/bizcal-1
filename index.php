3f46fa6a16aa2015127937ec11ffa9ad2258430f
kuhaha <kevin.yr.cheng@gmail.com>
2023-07-24 18:11:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,15 @@ use Exception;
 define ('DAT_DIR', 'dat');
 
 // header('Content-Type: text/plain; charset=UTF-8');
-$year  = (int)isset($_GET['y']) ? $_GET['y'] : 2023;
-$month = (int)isset($_GET['m']) ? $_GET['m'] :8;
+$year  = isset($_GET['y']) ? (int)$_GET['y'] : 2023;
+$month = isset($_GET['m']) ? (int)$_GET['m'] : 8;
 
 $schedule = [];
+$ac_year = ($month < 4) ? $year  -1 : $year; 
 try {
-    $schedule = Yaml::parseFile(DAT_DIR . "/schedule/cal{$year}.yaml");
+    $schedule = Yaml::parseFile(DAT_DIR . "/schedule/cal{$ac_year}.yaml");
 }catch (Exception $e){} 
+// echo json_encode($schedule, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE); 
 
 $dat_holiday = Yaml::parseFile(DAT_DIR . "/holiday.yaml");
 $holiday = new KsHoliday($year, $dat_holiday);
@@ -40,26 +42,28 @@ $calendar = new KsCalendar($year, $month);
 <?php
 printf("<h2>%d年%d月</h2>\n", $calendar->year, $calendar->month);
 
-echo printMonth($year, $month,$holiday, $schedule);
+echo printMonth($year, $month, $holiday, $schedule);
 
 function printMonth($year, $month, $holiday, $schedule)
 {
     $cal = new KsCalendar($year, $month);
     $hdays = $holiday->getHolidays($month);
+
     $sdays = [];
      if (isset($schedule[$year][$month])){
         foreach ($schedule[$year][$month] as $d=>$info){
-        if (isset($info['tag'])){
-            if (in_array($info['tag'], ['workday','lecture','extra'])){
-                $sdays[$d] = ['class'=>'workday', 'name'=>$info['name']]; 
+            if (isset($info['tag'])){
+                if (in_array($info['tag'], ['workday','lecture','extra'])){
+                    $sdays[(int)$d] = ['class'=>'workday', 'name'=>$info['name']]; 
+                }
+                if (in_array($info['tag'], ['holiday','vacation'])){
+                    $sdays[(int)$d] = ['class'=>'vacation', 'name'=>$info['name']]; 
+                }
+            }else{
+                $sdays[(int)$d] = ['class'=>'sat', 'name'=>$info['name']];
             }
-            if (in_array($info['tag'], ['holiday','vacation'])){
-                $sdays[$d] = ['class'=>'vacation', 'name'=>$info['name']]; 
-            }
-
         }
     }
-     }
     $names = $cal->getWeekdays();
     $days = $cal->getDays(); 
 
@@ -80,7 +84,7 @@ function printMonth($year, $month, $holiday, $schedule)
             $class .= ' holiday'; 
         }
         if (isset($sdays[$d])){
-            $class .= $sdays[$d]['class']; 
+            $class .= ' ' . $sdays[$d]['class']; 
         }
         $row .=  sprintf ('<td class="%s">%d</td>', $class, $d);
         if ($w == 6){
@@ -93,18 +97,36 @@ function printMonth($year, $month, $holiday, $schedule)
     }
     $out .=  "<tr>{$row}</tr>\n";
     $out = "<table>\n{$out}</table>\n";
-
-    $daynames = [];
+ 
+    $hdaynames = [];
     foreach ($hdays as $md=>$name){
         $d = (int)substr($md,-2);
-        $daynames[$d] =[ 'class'=>'holiday', 'name'=>$name]; 
+        $hdaynames[$d] =[ 'class'=>'holiday', 'name'=>$name]; 
     }
+    $sdaynames = [];
     foreach ($sdays as $d=>$info){
-        $daynames[$d] =[ 'class'=>$info['class'], 'name'=>$info['name'] ]; 
+        $sdaynames[$d] =['class'=>$info['class'], 'name'=>$info['name'] ]; 
     }
-    ksort($daynames);
-    foreach ($daynames as $d=>$info){ 
-        $out .= '<span class="' . $info['class'] . '">' . $d . ': ' . $info['name'] . '</span><br>'; 
+    $daynames = [];
+    foreach (array_keys($hdaynames) as $d){
+        $daynames[] = $d;
+    } 
+    foreach (array_keys($sdaynames) as $d){
+        $daynames[] = $d;
+    } 
+    $daynames = array_unique($daynames);
+    sort($daynames);
+    foreach ($daynames as $d){
+        if (isset($sdaynames[$d])){
+            $info = $sdaynames[$d];
+            $out .= '<span class="' . $info['class'] . '">' . $d . ': ' . $info['name'] . "</span><br>\n"; 
+        } 
+        if (isset($hdaynames[$d])){
+            $info = $hdaynames[$d];
+            $out .= '<span class="' . $info['class'] . '">' . $d . ': ' . $info['name'] . "</span><br>\n"; 
+
+        } 
+
     }
     return $out;
 }
